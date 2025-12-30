8494441cc05da11a4ecb089fc321267d997dca11
db <davidboxan@seznam.cz>
2025-12-30 20:37:13 +0100

    created base for index page

---
+++ front-end/src/components/RoundedButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+    innerText: string
+}>();
+</script>
+
+<template>
+    <div class="rounded-button">
+        {{ props.innerText }}
+    </div>
+</template>
+
+<style scoped>
+.rounded-button {
+    border-radius: 1.5rem;
+    padding: 0.5rem 1.5rem;
+    font-weight: 600;
+    background: linear-gradient(to right, #42D3F2, #05DF72);
+    width: fit-content;
+    outline: 2px solid transparent;
+    transition: outline 0.3s ease;
+    cursor: pointer;
+    display: inline-block;
+}
+.rounded-button:hover {
+    outline: 2px solid #53EAFD;
+}
+</style>
--- front-end/src/pages/HomePage.vue
+++ front-end/src/pages/HomePage.vue
@@ -1,15 +1,38 @@
 <script setup>
+import RoundedButton from "../components/RoundedButton.vue";
 </script>
 
 <template>
-    <div class="heading-wrapper">
-        <h1>Hello</h1>
+    <div class="homepage-content">
+        <div class="heading-wrapper">
+            <h1>Pitch Trainer</h1>
+        </div>
+        <div class="main-wrapper">
+            <p>On this page you can train your relative pitch hearing.</p>
+            <p>You will be played two notes, a <b>base</b> note from which you will have to guess the <b>second</b> note.</p>
+            <RoundedButton inner-text="Start"/>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.homepage-content {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 1rem;
+}
 .heading-wrapper {
     width: 100%;
+    padding: 0.5rem;
+    text-align: center;
+}
+.main-wrapper {
+    width: 100%;
+    max-width: 800px;
     padding: 0.5rem;
     text-align: center;
 }
