43dc15e17999ea307cc0828d8559e736653dce5f
db <davidboxan@seznam.cz>
2026-01-11 00:13:55 +0100

    created base script file for melody generation

---
+++ front-end/src/utils/melody.ts
@@ -0,0 +1,17 @@
+function getRandomNumber(max: number) {
+    return Math.floor(Math.random() * max);
+}
+
+function generateMelody(noteList: string[], melodyLength: number): string[] {
+    const listLength: number = noteList.length;
+    if (listLength === 0 || melodyLength <= 0) return [];
+
+    const melody: string[] = [];
+    for (let i = 0; i < melodyLength; i++) {
+        const idx = getRandomNumber(listLength);
+        const newNote = noteList[idx]!;
+        melody.push(newNote);
+    }
+
+    return melody;
+}
